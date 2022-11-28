92c2387db6868073b4db42a255a7f4a720ab3961
AmazyJoe <ochiengolu@gmail.com>
2022-11-28 16:17:25 +0300

    Arithmetic done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1=31,
+   num2=2;
+const multiply=num1 * num2;
 
-// Write your code here
+const random=Math.floor(Math.random() * 100);
+
+ let num3=32,
+ num4=7;
+ const mod=num3 % num4;
+
+ const max=Math.max(5,16,11,20);
